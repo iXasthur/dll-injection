edfee743eaf576e88c1a9f0a7c097d15e59c2b80
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-10-21 18:34:50 +0300

    Update main.cpp

--- hunter/main.cpp
+++ hunter/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include "windows.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    pid_t pid = 2400;
+    std::string stringToReplace = "12345";
+    std::string stringToReplaceTo = "I wanna be a multiplexor";
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+    if (processHandle != nullptr) {
+        MEMORY_BASIC_INFORMATION memInfo;
+
+        unsigned char *addr = nullptr;
+        while (VirtualQueryEx(processHandle, addr, &memInfo, sizeof(memInfo))) {
+            if (memInfo.State == MEM_COMMIT && (memInfo.Type == MEM_MAPPED || memInfo.Type == MEM_PRIVATE)) {
+                size_t bytesRead;
+                std::vector<char> buffer;
+                buffer.resize(memInfo.RegionSize);
+                ReadProcessMemory(processHandle, addr, buffer.data(), memInfo.RegionSize, &bytesRead);
+                buffer.resize(bytesRead);
+
+                auto offset = std::search(buffer.begin(), buffer.end(), stringToReplace.begin(), stringToReplace.end());
+                if (offset != buffer.end()) {
+                    int distance = std::distance(buffer.begin(), offset);
+                    LPVOID addrToWriteTo = addr + distance;
+
+                    std::cout << "Found string at " << addrToWriteTo << std::endl;
+
+                    if (WriteProcessMemory(processHandle, addrToWriteTo, stringToReplaceTo.data(), stringToReplaceTo.length() + 1,
+                                           nullptr)) {
+                        std::cout << "Changed" << std::endl;
+                    } else {
+                        DWORD error = GetLastError();
+                        std::cout << "Error " << error << std::endl;
+                    }
+                }
+            }
+            addr += memInfo.RegionSize;
+        }
+
+        CloseHandle(processHandle);
+    } else {
+        std::cout << "Invalid pid " << pid << std::endl;
+    }
+
     return 0;
 }
